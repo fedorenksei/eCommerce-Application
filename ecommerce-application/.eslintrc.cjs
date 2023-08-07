444c45e43bda43c1de8a9e19670706a5892cbfa2
Color-zebra <95714282+Color-zebra@users.noreply.github.com>
2023-08-07 18:38:24 +0300

    5.1, 5.4, 1.6, 1.4.1, 1.4.3 Environment configuration, README.
    * feat[5.1]: add storybook to project
    
    * feat[1.4.3]: add jest, sass and react-testing-library
    
    * feat[1.4]: configure eslint and prettier
    
    * feat[5.4]: add and configure tailwind
    
    * feat[1.6]: Comprehensive readme
    
    * fix: add missing new lines in the end of files
    
    * fix[1.6]: fix readme grammatic
    
    * feat[1.1.2]: add basic FSD folders structure

--- ecommerce-application/.eslintrc.cjs
+++ ecommerce-application/.eslintrc.cjs
@@ -1,18 +1,36 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:storybook/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'jsx-a11y'],
   rules: {
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/jsx-key': 'error',
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      {
+        allowConstantExport: true,
+      },
     ],
   },
-}
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};

2a0655a3952b19807cbe5596f10bba87fb6ddf58
Romanenkov Dmitriy <215rd@mail.ru>
2023-08-10 06:48:33 +0300

    feat: add tests

---
+++ ecommerce-application/src/app/App.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { AppComponent } from './model';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
+
+test('component just rendering', async () => {
+  render(<AppComponent />, { wrapper: BrowserRouter });
+
+  expect(screen.getByText('Main page')).toBeInTheDocument();
+
+  await userEvent.click(screen.getByText('Login'));
+  expect(screen.getByText('Login page')).toBeInTheDocument();
+
+  await userEvent.click(screen.getByText('Registration'));
+  expect(screen.getByText('Registration page')).toBeInTheDocument();
+});
+
+test('landing bad page', async () => {
+  const badRoute = '/verywronglink';
+
+  render(
+    <MemoryRouter initialEntries={[badRoute]}>
+      <AppComponent />
+    </MemoryRouter>
+  );
+
+  expect(screen.getByText('NotFound')).toBeInTheDocument();
+});

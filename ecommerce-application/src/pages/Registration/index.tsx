828efc2a9a4c83371396333315210f8dbeac99fd
Romanenkov Dmitriy <215rd@mail.ru>
2023-08-12 10:38:55 +0300

    feat[2.2.1]: add fields for first step of form

--- ecommerce-application/src/pages/Registration/index.tsx
+++ ecommerce-application/src/pages/Registration/index.tsx
@@ -1,5 +1,34 @@
 import React from 'react';
 
 export const Registration = () => {
-  return <div className="text-center text-4xl">Registration page</div>;
+  return (
+    <div className="bg-slate-800 p-10 text-white">
+      <form className="flex flex-col gap-10 items-center w-96 mx-auto">
+        <label className="flex justify-between w-full gap-3">
+          Email
+          <input type="text" />
+        </label>
+        <label className="flex justify-between w-full gap-3">
+          Password
+          <input type="text" />
+        </label>
+        <label className="flex justify-between w-full gap-3">
+          Password repeat
+          <input type="text" />
+        </label>
+        <label className="flex justify-between w-full gap-3">
+          First name
+          <input type="text" />
+        </label>
+        <label className="flex justify-between w-full gap-3">
+          Last name
+          <input type="text" />
+        </label>
+        <label className="flex justify-between w-full gap-3">
+          Date of birth
+          <input type="date" />
+        </label>
+      </form>
+    </div>
+  );
 };

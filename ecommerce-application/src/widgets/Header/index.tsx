70f2ce7a65f3050f02bc1e4e00936ce391af7f73
Romanenkov Dmitriy <215rd@mail.ru>
2023-08-19 23:38:45 +0300

    feat[2.5]: add desktop styles

--- ecommerce-application/src/widgets/Header/index.tsx
+++ ecommerce-application/src/widgets/Header/index.tsx
@@ -7,6 +7,7 @@ import { Registration } from '../../pages/Registration';
 import { Login } from '../../pages/Login';
 import { NotFound } from '../../pages/NotFound';
 import { ServerAPI } from '../../shared/api/ServerAPI';
+import { CustomerData } from '../../shared/types/interfaces';
 
 export const Header = () => {
   const auth = useSelector((state: RootState) => state.auth);
@@ -15,32 +16,45 @@ export const Header = () => {
 
   return (
     <>
-      <header>
-        <ul className="flex py-6 justify-around">
-          <li>
+      <header className="shadow-md bg-color flex justify-between items-center px-2">
+        <div className="flex items-center">
+          <div className="flex justify-center items-center">
+            <img
+              className="w-14 object-cover"
+              src="./public/images/logo.png"
+              alt="bike logo"
+            />
+          </div>
+          <div className="text-text-color text-2xl font-bold">BI-KU-KLE</div>
+        </div>
+        <ul className="flex py-6 gap-4">
+          <li className="text-center text-second-text-color text-sm font-bold hover:text-primary-color hover:underline">
             <Link to="/">Main</Link>
           </li>
           {!auth.isAuth && (
-            <li>
+            <li className="text-center text-second-text-color text-sm font-bold hover:text-primary-color hover:underline">
               <Link to="/login">Login</Link>
             </li>
           )}
           {!auth.isAuth && (
-            <li>
+            <li className="text-center text-second-text-color text-sm font-bold hover:text-primary-color hover:underline">
               <Link to="/registration">Registration</Link>
             </li>
           )}
           {auth.isAuth && (
-            <li>
-              <button onClick={() => serverAPI.logout()}>Logout</button>
+            <li className="text-center text-second-text-color text-sm font-bold hover:text-primary-color">
+              <button onClick={() => serverAPI.logout()}>
+                <span className="hover:underline">Logout</span>
+              </button>
             </li>
           )}
         </ul>
+        {customerData.customerData && (
+          <span className="text-center text-primary-color text-base font-bold">
+            {(customerData.customerData as CustomerData).email}
+          </span>
+        )}
       </header>
-      {<div>Is user login: {String(auth.isAuth)}</div>}
-      {customerData.customerData && (
-        <div>{JSON.stringify(customerData.customerData)}</div>
-      )}
       <Routes>
         <Route
           path="/"

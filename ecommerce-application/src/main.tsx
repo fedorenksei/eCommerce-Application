c9f91495b005f34dce4a574900565bb6fe2c8b44
Aleksei Fedorenko <117395871+fedorenksei@users.noreply.github.com>
2023-08-21 23:44:03 +0400

    feat[2]: add password eye feature
    * feat[2]: add eye icon for password showing
    
    * fix[2]: remove unneeded code

--- ecommerce-application/src/main.tsx
+++ ecommerce-application/src/main.tsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './app';
 
-if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  document.documentElement.classList.add('dark');
-}
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <>{App}</>

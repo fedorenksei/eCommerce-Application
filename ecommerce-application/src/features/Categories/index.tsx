a347f4f94fddff71b39fcdd66265a1981ac4a473
Romanenkov Dmitriy <215rd@mail.ru>
2023-08-30 21:17:27 +0300

    feat[3.0]: add changing url on category click

--- ecommerce-application/src/features/Categories/index.tsx
+++ ecommerce-application/src/features/Categories/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
-/* interface Props {
+interface Props {
   name: string;
   onClick: () => void;
-} */
+}
 
-/* const Category = ({ name, onClick }: Props) => {
-  return <button onClick={() => onClick()}>{name}</button>;
-}; */
+const Category = ({ name, onClick }: Props) => {
+  return (
+    <button
+      className="border-slate-800 border-2 cursor-pointer select-none"
+      onClick={() => onClick()}
+    >
+      {name}
+    </button>
+  );
+};
 
 interface testCat {
   name: string;
@@ -34,20 +42,27 @@ const testCategories: testCat[] = [
 
 export const Categories = () => {
   const [categories, setCategories] = useState<testCat[]>([]);
+  const { category } = useParams();
+  console.log(category);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     setCategories(() => testCategories);
   }, []);
+
+  const onCategoryClick = (categoryName: string) => {
+    navigate(`/catalog/${categoryName}`);
+  };
   return (
     <div className="flex w-full justify-around">
       {categories.map((category) => {
         return (
-          <div
-            className="border-slate-800 border-2 cursor-pointer select-none"
+          <Category
+            name={category.name}
+            onClick={() => onCategoryClick(category.name)}
             key={category.name}
-          >
-            {category.name}
-          </div>
+          />
         );
       })}
     </div>

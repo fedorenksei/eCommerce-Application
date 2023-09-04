004c0bf6516e8a19f5227ececb04989062497708
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-09-05 02:35:36 +0300

    feat(3.2): add basic layout of product page

--- ecommerce-application/src/pages/Product/model/Product.tsx
+++ ecommerce-application/src/pages/Product/model/Product.tsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import { DetailedProductData } from '../../../shared/types/interfaces';
 import { ServerAPI } from '../../../shared/api/ServerAPI';
+import { Header2 } from '../../../shared/ui/text/Header2';
+import { Header3 } from '../../../shared/ui/text/Header3';
+import { Paragraph } from '../../../shared/ui/text/Paragraph';
 
 export const Product = () => {
   const { id } = useParams();
@@ -23,33 +26,36 @@ export const Product = () => {
     fetchProduct();
   }, [cbSetProduct, id, serverApi]);
 
+  const productName = product?.masterData.current.name['en-US'];
+  const description = product?.masterData.current.description['en-US'];
+  const imageUrls = product?.masterData.current.masterVariant.images || [];
+  const price =
+    Number(
+      product?.masterData.current.masterVariant.prices[0].value.centAmount,
+    ) / 100;
+  const discountedPrice = Math.floor(price * 0.95);
+
   return (
-    <div className="flex flex-col">
-      <span>
-        <span className="font-bold">name:</span>{' '}
-        {product?.masterData.current.name['en-US']}
-      </span>
-      <span>
-        <span className="font-bold">id:</span> {product?.id}
-      </span>
-      <span>
-        <span className="font-bold">description:</span>{' '}
-        {product?.masterData.current.description['en-US']}
-      </span>
-      <span>
-        <span className="font-bold">image links:</span>{' '}
-        <div className="flex flex-col"></div>
-        {product?.masterData.current.masterVariant.images.map(({ url }) => (
-          <div key={url}>{url}</div>
-        ))}
-      </span>
-      <span>
-        <span className="font-bold">price:</span>{' '}
-        {Number(
-          product?.masterData.current.masterVariant.prices[0].value.centAmount,
-        ) / 100}{' '}
-        <span className="font-bold">euro</span>
-      </span>
+    <div className="w-full space-y-4 p-10">
+      <div className="text-center">
+        <Header2>Product Info</Header2>
+      </div>
+      <div className="max-w-lg space-y-2 mx-auto">
+        <Header3>{productName}</Header3>
+        <div className="space-x-2">
+          <span className="text-neutral-400 line-through">€{price}</span>
+          <span className="text-danger-color">€{discountedPrice}</span>
+        </div>
+        <Paragraph>{description}</Paragraph>
+      </div>
+
+      {imageUrls.map(({ url }) => (
+        <img
+          src={url}
+          alt={productName}
+          key={url}
+        />
+      ))}
     </div>
   );
 };

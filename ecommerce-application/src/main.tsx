6cfc3be6803630a1c876cf765677bae32881f750
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-09-05 03:22:35 +0300

    fix(3): move preflight to main.tsx

--- ecommerce-application/src/main.tsx
+++ ecommerce-application/src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './app';
+import { ServerAPI } from './shared/api/ServerAPI';
+
+const serverAPI = ServerAPI.getInstance();
+await serverAPI.preflight();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

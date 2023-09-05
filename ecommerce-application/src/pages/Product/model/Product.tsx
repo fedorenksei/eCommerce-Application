0d68431b06e601804b704becd72d69e3e405dd7c
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-09-05 03:13:04 +0300

    feat(3.2): add image slider of product page

--- ecommerce-application/src/pages/Product/model/Product.tsx
+++ ecommerce-application/src/pages/Product/model/Product.tsx
@@ -5,6 +5,8 @@ import { ServerAPI } from '../../../shared/api/ServerAPI';
 import { Header2 } from '../../../shared/ui/text/Header2';
 import { Header3 } from '../../../shared/ui/text/Header3';
 import { Paragraph } from '../../../shared/ui/text/Paragraph';
+import { getButtonStyles } from '../../../shared/ui/styles';
+import clsx from 'clsx';
 
 export const Product = () => {
   const { id } = useParams();
@@ -28,12 +30,15 @@ export const Product = () => {
 
   const productName = product?.masterData.current.name['en-US'];
   const description = product?.masterData.current.description['en-US'];
-  const imageUrls = product?.masterData.current.masterVariant.images || [];
   const price =
     Number(
       product?.masterData.current.masterVariant.prices[0].value.centAmount,
     ) / 100;
   const discountedPrice = Math.floor(price * 0.95);
+
+  const imageUrls = product?.masterData.current.masterVariant.images || [];
+  const [currImg, setCurrImg] = useState(0);
+  const currImgUrl = imageUrls[currImg]?.url;
 
   return (
     <div className="w-full space-y-4 p-10">
@@ -49,13 +54,51 @@ export const Product = () => {
         <Paragraph>{description}</Paragraph>
       </div>
 
-      {imageUrls.map(({ url }) => (
-        <img
-          src={url}
-          alt={productName}
-          key={url}
-        />
-      ))}
+      <div
+        className={clsx(
+          'max-w-[100%] gap-5 p-4',
+          'flex justify-center items-center',
+        )}
+      >
+        <button
+          disabled={currImg === 0 ? true : false}
+          onClick={() => {
+            setCurrImg((c) => c - 1);
+          }}
+          className={clsx(
+            getButtonStyles({
+              size: 'small',
+              filling: 'transparent',
+              shape: 'round',
+              disabled: currImg === 0 ? true : false,
+            }),
+          )}
+        >
+          &lt;&lt;
+        </button>
+
+        <button
+          disabled={currImg >= imageUrls.length - 1 ? true : false}
+          onClick={() => {
+            setCurrImg((c) => c + 1);
+          }}
+          className={getButtonStyles({
+            size: 'small',
+            filling: 'transparent',
+            shape: 'round',
+            disabled: currImg >= imageUrls.length - 1 ? true : false,
+          })}
+        >
+          &gt;&gt;
+        </button>
+      </div>
+
+      <img
+        src={currImgUrl}
+        alt={productName}
+        key={`${id}_image_${currImg}`}
+        className="md:max-w-[80%] mx-auto"
+      />
     </div>
   );
 };

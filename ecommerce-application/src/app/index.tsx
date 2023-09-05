43733285733438101cb935f8b05c31b3bcfe6261
Romanenkov Dmitriy <215rd@mail.ru>
2023-09-05 12:24:51 +0300

    fix[3.0]: remove useless useEffect

--- ecommerce-application/src/app/index.tsx
+++ ecommerce-application/src/app/index.tsx
@@ -1,16 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Header } from '../widgets/Header';
 import { withProviders } from './providers';
-import { ServerAPI } from '../shared/api/ServerAPI';
 import { Modal } from '../shared/ui/modals';
 
 const AppComponent = () => {
-  const serverAPI = ServerAPI.getInstance();
-
-  useEffect(() => {
-    serverAPI.preflight();
-  }, [serverAPI]);
-
   return (
     <>
       <Header />

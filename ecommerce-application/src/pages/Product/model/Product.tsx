fa75be7f52015fa3a6ff52af11cb5d487cb55c48
Aleksei Fedorenko <117395871+fedorenksei@users.noreply.github.com>
2023-09-06 19:37:45 +0300

    feat[3.3]: add detailed Product page
    * feat[3.3]: add route and empty component
    
    * fix[3.3]: fix getting param
    
    * feat[3.3]: get data for next component creating
    
    * fix[3.3]: fix price
    
    * fix: fix links section
    
    * feat(3.2): add basic layout of product page
    
    * feat(3.2): add image slider of product page
    
    * feat(3.3): make slider
    
    * feat(3.3): make popup
    
    * feat(3.3): make popup full-scale slider
    
    ---------
    
    Co-authored-by: Romanenkov Dmitriy <215rd@mail.ru>

---
+++ ecommerce-application/src/pages/Product/model/Product.tsx
@@ -0,0 +1,133 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
+import { DetailedProductData } from '../../../shared/types/interfaces';
+import { ServerAPI } from '../../../shared/api/ServerAPI';
+import { Header2 } from '../../../shared/ui/text/Header2';
+import { Header3 } from '../../../shared/ui/text/Header3';
+import { Paragraph } from '../../../shared/ui/text/Paragraph';
+import { getButtonStyles } from '../../../shared/ui/styles';
+import clsx from 'clsx';
+
+import './slider.css';
+import { useToggle } from '../../../shared/utils/hooks';
+
+export const Product = () => {
+  const { id } = useParams();
+  const [product, setProduct] = useState<DetailedProductData | null>(null);
+
+  const cbSetProduct = useCallback(
+    (productData: DetailedProductData) => setProduct(productData),
+    [],
+  );
+  const serverApi = ServerAPI.getInstance();
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      if (!id) return;
+      const fetchedProduct = await serverApi.getProduct(id);
+      cbSetProduct(fetchedProduct);
+    };
+
+    fetchProduct();
+  }, [cbSetProduct, id, serverApi]);
+
+  const productName = product?.masterData.current.name['en-US'];
+  const description = product?.masterData.current.description['en-US'];
+  const price =
+    Number(
+      product?.masterData.current.masterVariant.prices[0].value.centAmount,
+    ) / 100;
+  const discountedPrice = Math.floor(price * 0.95);
+
+  const imageUrls =
+    product?.masterData.current.masterVariant.images.map(
+      (image) => image.url,
+    ) || [];
+  const [currImg, setCurrImg] = useState(0);
+  const [fullScreen, toggleFullScreen] = useToggle();
+
+  return (
+    <div className="w-full space-y-4 p-10">
+      <div className="text-center">
+        <Header2>Product Info</Header2>
+      </div>
+      <div className="max-w-lg space-y-2 mx-auto">
+        <Header3>{productName}</Header3>
+        <div className="space-x-2">
+          <span className="text-neutral-400 line-through">€{price}</span>
+          <span className="text-danger-color">€{discountedPrice}</span>
+        </div>
+        <Paragraph>{description}</Paragraph>
+
+        <div
+          className={clsx(
+            'space-y-3 text-center pb-3',
+            fullScreen &&
+              'absolute top-0 left-0 shadow-[0px_0px_0px_100000px_rgba(0,_0,_0,_0.5)] z-10 bg-bg-color dark:bg-dt-bg-color',
+          )}
+        >
+          <div
+            className={clsx(
+              'max-w-[100%] gap-5 p-4',
+              'flex justify-center items-center',
+            )}
+          >
+            <button
+              disabled={currImg === 0 ? true : false}
+              onClick={() => {
+                setCurrImg((c) => c - 1);
+              }}
+              className={clsx(
+                getButtonStyles({
+                  size: 'small',
+                  filling: 'transparent',
+                  shape: 'round',
+                  disabled: currImg === 0 ? true : false,
+                }),
+              )}
+            >
+              &lt;&lt;
+            </button>
+
+            <button
+              disabled={currImg >= imageUrls.length - 1 ? true : false}
+              onClick={() => {
+                setCurrImg((c) => c + 1);
+              }}
+              className={getButtonStyles({
+                size: 'small',
+                filling: 'transparent',
+                shape: 'round',
+                disabled: currImg >= imageUrls.length - 1 ? true : false,
+              })}
+            >
+              &gt;&gt;
+            </button>
+          </div>
+          <div
+            className="overflow-hidden"
+            role="presentation"
+            onClick={toggleFullScreen}
+          >
+            <div
+              className="w-[100%] cursor-pointer flex transition-all my-slider-translate"
+              style={{ '--slide-number': currImg } as React.CSSProperties}
+            >
+              {imageUrls.map((url, i) => (
+                <img
+                  src={url}
+                  alt={productName}
+                  key={`${id}_image_${i}`}
+                  className="w-[100%] mx-auto"
+                />
+              ))}
+            </div>
+          </div>
+          <Paragraph>
+            Click on the image to {fullScreen ? 'go back' : 'see it full-scale'}
+          </Paragraph>
+        </div>
+      </div>
+    </div>
+  );
+};

19b0fdbce35fe3c2fec1503fdb241b1e387806e8
Aleksei Fedorenko <117395871+fedorenksei@users.noreply.github.com>
2023-09-08 11:27:52 +0400

    fix[3]: fix issues after sprint 3
    * fix[3]: fix searching and sorting by brand
    
    * fix[3]: fix sorting options naming
    
    * fix[3]: display price in euros in range filter
    
    * refactor[3]: delete unneeded files

--- ecommerce-application/src/entities/CatalogSortPanel/model/CatalogSortPanel.tsx
+++ ecommerce-application/src/entities/CatalogSortPanel/model/CatalogSortPanel.tsx
@@ -28,8 +28,8 @@ export const CatalogSortPanel = () => {
         <option value="">default</option>
         <option value="nameAsc">name &#40;A =&gt; Z&#41;</option>
         <option value="nameDesc">name &#40;Z =&gt; A&#41;</option>
-        <option value="priceAsc">price max =&gt; min</option>
-        <option value="priceDesc">price min =&gt; max</option>
+        <option value="priceAsc">price min =&gt; max</option>
+        <option value="priceDesc">price max =&gt; min</option>
       </select>
     </div>
   );

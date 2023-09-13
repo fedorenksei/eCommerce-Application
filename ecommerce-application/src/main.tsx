8d185b075f8ed6484b5bbf3c4261e6dd1a2db677
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-09-13 23:25:21 +0300

    perf[4]: use async IIFE in main.tsx

--- ecommerce-application/src/main.tsx
+++ ecommerce-application/src/main.tsx
@@ -1,27 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './app';
 import { ServerAPI } from './shared/api/ServerAPI';
 
-type Props = {
-  component: JSX.Element;
-};
-export const Init = ({ component }: Props) => {
+(async () => {
   const serverAPI = ServerAPI.getInstance();
-  const [isReady, setIsReady] = useState<boolean>(false);
-  useEffect(() => {
-    const start = async () => {
-      await serverAPI.init();
-      setIsReady(true);
-    };
-    start();
-  }, [serverAPI]);
-  return <>{isReady ? component : ''}</>;
-};
+  await serverAPI.init();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Init component={App} />
-  </React.StrictMode>,
-);
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <>{App}</>
+    </React.StrictMode>,
+  );
+})();

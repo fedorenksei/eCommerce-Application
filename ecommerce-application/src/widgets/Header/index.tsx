f1661e32071d7f6dbd8ba4f97cd14f5ef8b723e5
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-09-16 19:18:01 +0300

    Revert "feat[4.6]: install icons, use in header"
    This reverts commit d4c8dcd5b2914a86d130838fd57087aaafc408f6.

--- ecommerce-application/src/widgets/Header/index.tsx
+++ ecommerce-application/src/widgets/Header/index.tsx
@@ -1,99 +1,99 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../app/store';
 import { ServerAPI } from '../../shared/api/ServerAPI';
-import {
-  CartOutline,
-  HomeOutline,
-  LogInOutline,
-  LogOutOutline,
-  PersonAddOutline,
-  PersonOutline,
-  StorefrontOutline,
-} from 'react-ionicons';
-import { BikukleLogo } from './ui/BikukleLogo';
-import { Ionicon } from '../../shared/types/icon';
-
-interface NavItemData {
-  link?: string;
-  onClick?: () => void;
-  title: string;
-  icon: Ionicon;
-  text?: string;
-}
-
-import { NavItem } from './ui/NavItem';
+import { CustomerData } from '../../shared/types/interfaces';
 
 export const Header = () => {
   const auth = useSelector((state: RootState) => state.auth);
+  const customerData = useSelector((state: RootState) => state.customerData);
   const serverAPI = ServerAPI.getInstance();
   const [burgerShown, setBurgerShown] = useState(false);
-
-  const commonNavItems: NavItemData[] = [
-    {
-      link: '/',
-      title: 'Main page',
-      icon: HomeOutline,
-      text: 'Main',
-    },
-    {
-      link: '/catalog',
-      title: 'Catalog',
-      icon: StorefrontOutline,
-      text: 'Catalog',
-    },
-    {
-      link: '/basket',
-      title: 'Cart',
-      icon: CartOutline,
-    },
-  ];
-
-  const anonymousNavItems: NavItemData[] = [
-    {
-      link: '/login',
-      title: 'Log In',
-      icon: LogInOutline,
-    },
-    {
-      link: '/registration',
-      title: 'Sign up',
-      icon: PersonAddOutline,
-    },
-  ];
-
-  const authorizedNavItems: NavItemData[] = [
-    {
-      link: '/profile',
-      title: 'Profile',
-      icon: PersonOutline,
-    },
-    {
-      onClick: () => serverAPI.logout(),
-      title: 'Log out',
-      icon: LogOutOutline,
-    },
-  ];
 
   return (
     <>
       <header className="shadow-md z-10 bg-bg-color flex justify-between items-center px-2 h-20 relative">
-        <BikukleLogo />
+        <Link to="/">
+          <div className="flex items-center">
+            <div className="flex w-14 justify-center items-center">
+              <img
+                className="w-14 object-cover"
+                src="./images/logo.png"
+                alt="bike logo"
+              />
+            </div>
+            <div className="text-text-color text-2xl font-bold min-w-[8rem]">
+              BI-KU-KLE
+            </div>
+          </div>
+        </Link>
         <div
           className={`${
             !burgerShown ? 'hidden' : 'absolute flex flex-col top-20 left-0'
           } shadow-md md:shadow-none bg-bg-color w-full z-10 md:flex md:static md:flex-row justify-end gap-10 items-center flex-auto`}
         >
           <ul className="flex py-6 gap-4 flex-col md:flex-row">
-            {commonNavItems
-              .concat(auth.isAuth ? authorizedNavItems : anonymousNavItems)
-              .map((item) => (
-                <NavItem
-                  burgerAction={() => setBurgerShown((state) => !state)}
-                  key={item.title}
-                  {...item}
-                />
-              ))}
+            <li className="text-center text-text-color dark:text-dt-text-color text-md font-bold hover:text-primary-color hover:underline">
+              <Link
+                onClick={() => setBurgerShown((state) => !state)}
+                to="/"
+              >
+                Main
+              </Link>
+            </li>
+            <li className="text-center text-text-color dark:text-dt-text-color text-md font-bold hover:text-primary-color hover:underline">
+              <Link
+                onClick={() => setBurgerShown((state) => !state)}
+                to="/catalog"
+              >
+                Catalog
+              </Link>
+            </li>
+          </ul>
+          <ul className="flex py-6 gap-4 flex-col md:flex-row">
+            {!auth.isAuth && (
+              <>
+                <li className="text-center text-text-color dark:text-dt-text-color text-md font-bold hover:text-primary-color hover:underline">
+                  <Link
+                    onClick={() => setBurgerShown((state) => !state)}
+                    to="/login"
+                  >
+                    Log in
+                  </Link>
+                </li>
+                <li className="text-center text-text-color dark:text-dt-text-color text-md font-bold hover:text-primary-color hover:underline">
+                  <Link
+                    onClick={() => setBurgerShown((state) => !state)}
+                    to="/registration"
+                  >
+                    Sign up
+                  </Link>
+                </li>
+              </>
+            )}
+            {auth.isAuth && (
+              <>
+                <li className="text-center text-text-color dark:text-dt-text-color text-md font-bold hover:text-primary-color">
+                  <button onClick={() => serverAPI.logout()}>
+                    <span className="hover:underline">Logout</span>
+                  </button>
+                </li>
+                <li className="text-center text-text-color dark:text-dt-text-color text-md font-bold hover:text-primary-color hover:underline">
+                  <Link
+                    onClick={() => setBurgerShown((state) => !state)}
+                    to="/profile"
+                  >
+                    Profile
+                  </Link>
+                </li>
+              </>
+            )}
+            {customerData.customerData && (
+              <span className="text-center text-primary-color text-base font-bold">
+                {(customerData.customerData as CustomerData).email}
+              </span>
+            )}
           </ul>
         </div>
         <button
